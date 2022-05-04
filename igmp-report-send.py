434624cd436c9b49fc6609f4b16c428f71621840
volody314 <volody314@yandex.ru>
2022-05-04 10:49:31 +1000

    dev1 В скриптах Python отправки тестовых сообщений испоравлены IP-адреса

--- igmp-report-send.py
+++ igmp-report-send.py
@@ -1,7 +1,7 @@
 #!python3
 import scapy.all as scapy
 
-pack = (scapy.IP(dst="224.0.0.1",proto=2)/'\x16\x00\x00\x00\xE0\x00\x00\x63')
+pack = (scapy.IP(dst="127.0.0.1",src="192.168.88.1",proto=2)/'\x16\x00\x00\x00\xE0\x00\x00\x63')
 del pack[scapy.IP].chksum
 pack.show2()
 scapy.sr1(pack)
