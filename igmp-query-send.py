9008aa509aebf345107d35436258e41316efacd0
volody314 <volody314@yandex.ru>
2022-05-04 15:32:12 +1000

    dev1 Результаты тестирования обмена по сети

--- igmp-query-send.py
+++ igmp-query-send.py
@@ -1,7 +1,7 @@
 #!python3
 import scapy.all as scapy
 
-pack = (scapy.IP(dst="127.0.0.1",src="192.168.88.1",proto=2)/'\x11\x00\x00\x00\xE0\x00\x00\x63')
+pack = (scapy.IP(dst="192.168.88.216",src="192.168.88.1",proto=2)/'\x11\x00\x00\x00\xE0\x00\x00\x63')
 del pack[scapy.IP].chksum
 pack.show2()
 scapy.sr1(pack)
